234194acf1b018ebd7b5db0478667f9001b704e4
Abdou <2000abdallah2014@gmail.com>
2023-01-27 12:46:40 +0100

    Added user feedback when profile isn't complete +some css improvments

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -18,14 +18,11 @@ const cartStore = useCartStore();
     <div class="flex-none">
       <div class="dropdown dropdown-end">
         <CartIcon :loading="cartStore.loading" :count="cartStore.count" />
-        <div
-          tabindex="0"
-          class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
-        >
+        <div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
           <div class="card-body">
-            <span class="font-bold text-lg">0 Items</span>
-            <span class="text-info"
-              >Subtotal: <ProductPrice :price="cartStore.subtotal" />
+            <span class="font-bold text-lg"> N of items : {{ cartStore.count }} </span>
+            <span class="text-info">Subtotal:
+              <ProductPrice :price="cartStore.subtotal" />
             </span>
             <div class="card-actions">
               <NuxtLink :to="{ name: 'cart' }">
@@ -38,14 +35,8 @@ const cartStore = useCartStore();
 
       <!--Buttons for UN-logged In Users-->
       <div v-if="!loggedInUser">
-        <NuxtLink
-          to="/login"
-          class="btn btn-ghost border-2 border-gray-100 btn-sm ml-5"
-          >Login</NuxtLink
-        >
-        <NuxtLink to="/register" class="btn btn-primary btn-sm ml-2"
-          >Register</NuxtLink
-        >
+        <NuxtLink to="/login" class="btn btn-ghost border-2 border-gray-100 btn-sm ml-5">Login</NuxtLink>
+        <NuxtLink to="/register" class="btn btn-primary btn-sm ml-2">Register</NuxtLink>
       </div>
 
       <!--UI for logged In Users-->
@@ -53,10 +44,7 @@ const cartStore = useCartStore();
         <label tabindex="0" class="btn btn-sm ml-5">
           <button>{{ loggedInUser.email }}</button>
         </label>
-        <ul
-          tabindex="0"
-          class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-        >
+        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
           <li>
             <a class="justify-between">
               Profile
@@ -64,7 +52,9 @@ const cartStore = useCartStore();
             </a>
           </li>
           <li><a>Settings</a></li>
-          <li><NuxtLink to="/logout">Logout</NuxtLink></li>
+          <li>
+            <NuxtLink to="/logout">Logout</NuxtLink>
+          </li>
         </ul>
       </div>
     </div>
--- pages/cart.vue
+++ pages/cart.vue
@@ -8,141 +8,158 @@ const showConfirmation = ref(false)
 
 const user = ref(Deskree.loggedInUser);
 
+function handleBuyClick() {
+
+  if (user.value.email && user.value.address && user.value.wilaya && user.value.phone_number) { showConfirmation.value = !showConfirmation.value }
+  else { useAlertsStore().warning("please complete your profile infos") }
+}
 function handleOrder() {
-  if (user.value.email && user.value.address && user.value.wilaya && user.value.phone_number) {
-    cartStore.products.forEach((product) => Deskree.orders.placeOrder(product))
-  }
-  else { console.log("complet the profile", user) }
-
-
+  cartStore.products.forEach((product) => Deskree.orders.placeOrder(product))
 }
 
 
 </script>
 <template>
 
-  <div class="m-10">
-    <CheckoutConfirmation v-if="showConfirmation" @toggleConformation="showConfirmation = !showConfirmation"
-      @placeOrder="handleOrder" />
+  <div>
+
+    <div class="m-10" v-if="cartStore.count">
+      <Transition name="fade">
+        <CheckoutConfirmation v-if="showConfirmation" @toggleConformation="showConfirmation = !showConfirmation"
+          @placeOrder="handleOrder" />
+      </Transition>
+
+      <h1 class="text-3xl mb-5 font-bold">Your Cart</h1>
+
+      <div class="md:flex w-full">
+        <div class="md:w-3/4">
 
 
-    <h1 class="text-3xl mb-5 font-bold">Your Cart</h1>
-    <div class="md:flex w-full">
-      <div class="md:w-3/4">
-        <!-- Use this markup to display an empty cart -->
-        <!-- <div  class="italic text-center pt-10">
-          Cart is empty
-        </div> -->
-        <div class="overflow-x-auto">
-          <div class="table w-full">
-            <table class="w-full">
-              <!-- head -->
-              <thead>
-                <tr>
-                  <th>
-                    <label>
-                      <input type="checkbox" class="checkbox" ref="checkAll" />
-                    </label>
-                  </th>
-                  <th></th>
-                  <th>Product</th>
-                  <th>Price</th>
-                  <th>Number of Items</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="product in cartStore.products" :key="product.sys.id">
-                  <th>
-                    <label>
-                      <input v-model="selected" type="checkbox" class="checkbox" @change="checkAll.checked = false"
-                        :value="product.sys.id" />
-                    </label>
-                  </th>
-                  <td>
-                    <div class="flex items-center space-x-3">
-                      <div class="avatar">
-                        <div class="mask mask-squircle w-12 h-12">
-                          <img :src="product.fields.image[0].fields.file.url"
-                            :alt="product.fields.image[0].fields.title" />
+          <div class="overflow-x-auto">
+            <div class="table w-full">
+              <table class="w-full">
+                <!-- head -->
+                <thead>
+                  <tr>
+                    <th>
+                      <label>
+                        <input type="checkbox" class="checkbox" ref="checkAll" />
+                      </label>
+                    </th>
+                    <th></th>
+                    <th>Product</th>
+                    <th>Price</th>
+                    <th>Number of Items</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="product in cartStore.products" :key="product.sys.id">
+                    <th>
+                      <label>
+                        <input v-model="selected" type="checkbox" class="checkbox" @change="checkAll.checked = false"
+                          :value="product.sys.id" />
+                      </label>
+                    </th>
+                    <td>
+                      <div class="flex items-center space-x-3">
+                        <div class="avatar">
+                          <div class="mask mask-squircle w-12 h-12">
+                            <img :src="product.fields.image[0].fields.file.url"
+                              :alt="product.fields.image[0].fields.title" />
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </td>
-                  <td>
-                    <div class="font-bold">
-                      {{ product.fields.name }}
-                    </div>
-                    <ProductHeat :heat-level="product.fields.heatLevel" />
-                  </td>
-                  <td>
-                    <ProductPrice :price="product.fields.price" />
-                  </td>
+                    </td>
+                    <td>
+                      <div class="font-bold">
+                        {{ product.fields.name }}
+                      </div>
+                      <ProductHeat :heat-level="product.fields.heatLevel" />
+                    </td>
+                    <td>
+                      <ProductPrice :price="product.fields.price" />
+                    </td>
 
-                  <td>
-                    <input class="input input-bordered w-20" type="number" v-model="product.count" />
-                  </td>
-                  <th>
-                    <NuxtLink :to="{
-                      name: 'products-id',
-                      params: { id: product.sys.id },
-                    }">
-                      <button class="btn btn-ghost btn-xs">details</button>
-                    </NuxtLink>
-                  </th>
-                </tr>
-              </tbody>
-            </table>
-            <button v-if="selected.length" class="text-sm text-red-500" @click="
+                    <td>
+                      <input class="input input-bordered w-20" type="number" v-model="product.count" />
+                    </td>
+                    <th>
+                      <NuxtLink :to="{
+                        name: 'products-id',
+                        params: { id: product.sys.id },
+                      }">
+                        <button class="btn btn-ghost btn-xs">details</button>
+                      </NuxtLink>
+                    </th>
+                  </tr>
+                </tbody>
+              </table>
+              <button v-if="selected.length" class="text-sm text-red-500" @click="
   cartStore.removeProducts(selected);
 selected = [];
-            ">
-              Remove Selected
-            </button>
+              ">
+                Remove Selected
+              </button>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="md:w-1/4 pl-5">
-        <div class="card bg-slate-50">
-          <div class="card-body">
-            <ul>
-              <li>
-                <strong>Subtotal</strong>:
-                <ProductPrice :price="cartStore.subtotal" />
-              </li>
-              <li>
-                <strong>Estimated Taxes </strong>:
-                <ProductPrice :price="cartStore.taxTotal" />
-              </li>
-              <li>
-                <strong>Total</strong>:
-                <ProductPrice :price="cartStore.total" />
-              </li>
-            </ul>
-            <div class="card-actions justify-end w-full flex-col my-10" v-if="Deskree.loggedInUser.value">
-              <p> Name : {{ Deskree.loggedInUser.value.email ? Deskree.loggedInUser.value.name : "not set" }}</p>
-              <p> phone number : <strong> {{
-                Deskree.loggedInUser.value.phone_number.length > 5 ?
-                  Deskree.loggedInUser.value.phone_number : "not set"
-              }}</strong></p>
-              <p> wilaya : <strong> {{
-                Deskree.loggedInUser.value.wilaya ? Deskree.loggedInUser.value.wilaya : "not set"
-              }}</strong></p>
-              <p> address : <strong> {{
-                Deskree.loggedInUser.value.address ? Deskree.loggedInUser.value.address : "not set"
-              }}</strong></p>
+        <div class="md:w-1/4 pl-5">
+          <div class="card bg-slate-50">
+            <div class="card-body">
+              <ul>
+                <li>
+                  <strong>Subtotal</strong>:
+                  <ProductPrice :price="cartStore.subtotal" />
+                </li>
+                <li>
+                  <strong>Estimated Taxes </strong>:
+                  <ProductPrice :price="cartStore.taxTotal" />
+                </li>
+                <li>
+                  <strong>Total</strong>:
+                  <ProductPrice :price="cartStore.total" />
+                </li>
+              </ul>
+              <div class="card-actions justify-end w-full flex-col my-10" v-if="Deskree.loggedInUser.value">
+                <p> Name : {{ Deskree.loggedInUser.value.email ? Deskree.loggedInUser.value.name : "not set" }}</p>
+                <p> phone number : <strong> {{
+                  Deskree.loggedInUser.value.phone_number.length > 5 ?
+                    Deskree.loggedInUser.value.phone_number : "not set"
+                }}</strong></p>
+                <p> wilaya : <strong> {{
+                  Deskree.loggedInUser.value.wilaya ? Deskree.loggedInUser.value.wilaya : "not set"
+                }}</strong></p>
+                <p> address : <strong> {{
+                  Deskree.loggedInUser.value.address ? Deskree.loggedInUser.value.address : "not set"
+                }}</strong></p>
 
-              <button class="btn btn-primary w-full" @click="showConfirmation = !showConfirmation">
-                BUY!
-              </button>
-              <NuxtLink to="user/profile" class="btn btn-primary w-full"> <button>
-                  Edit my infos
-                </button></NuxtLink>
+                <button class="btn btn-primary w-full" @click="handleBuyClick">
+                  BUY!
+                </button>
+                <NuxtLink to="user/profile" class="btn btn-primary w-full"> <button>
+                    Edit my infos
+                  </button></NuxtLink>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <div class="italic text-center text-3xl pt-10" v-else>
+      Cart is empty...
+    </div>
   </div>
 </template>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.4s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
