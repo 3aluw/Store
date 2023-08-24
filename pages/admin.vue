c0b71e26f82c0bde8152e8d6f46dcc19da35aff5
Abdou <2000abdallah2014@gmail.com>
2023-08-24 19:05:38 +0100

    added icons to aside admin dashboard

--- pages/admin.vue
+++ pages/admin.vue
@@ -4,11 +4,11 @@
         <div class="flex w-full">
             <aside>
                 <ul>
-                    <li class="nav-items">stats</li>
-                    <li class="nav-items">users</li>
-                    <li class="nav-items">orders</li>
-                    <li class="nav-items">reviews</li>
-                    <li class="nav-items">carts</li>
+                    <li class="nav-items flex gap-2"><img src="~/assets/icons/stats.svg" alt=""> stats</li>
+                    <li class="nav-items flex gap-2"><img src="~/assets/icons/user.svg" alt=""> users</li>
+                    <li class="nav-items flex gap-2"><img src="~/assets/icons/orders.svg" alt=""> orders</li>
+                    <li class="nav-items flex gap-2"><img src="~/assets/icons/star.svg" alt=""> reviews</li>
+                    <li class="nav-items flex gap-2"><img src="~/assets/icons/cart.svg" alt=""> carts</li>
                 </ul>
             </aside>
 
@@ -46,14 +46,16 @@ definePageMeta({
     border-bottom: solid 1px #68299F;
 }
 
-aside {
-    padding-inline: 2rem;
+aside>ul {
+    padding-inline: 3rem;
+    padding-top: 5rem;
     display: flex;
     flex-direction: column;
     gap: 1rem;
     background: linear-gradient(180deg, rgba(255, 255, 255, 0.60) 0%, rgba(255, 255, 255, 0.30) 100%);
     box-shadow: 3px 0px 10px 0px rgba(0, 0, 0, 0.10);
     backdrop-filter: blur(110px);
+    color: #6D7D93;
 }
 
 main {
