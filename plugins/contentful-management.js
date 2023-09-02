da91f841cdb1e50802f1b8cb6b138b98c938749e
Abdou <2000abdallah2014@gmail.com>
2023-09-02 09:21:36 +0100

    added contentful-management and its plugin

---
+++ plugins/contentful-management.js
@@ -0,0 +1,25 @@
+import * as contentful from 'contentful-management';
+
+export default defineNuxtPlugin(() => {
+  const runtimeConfig = useRuntimeConfig()
+
+  
+  const scopedPlainClient = contentful.createClient(
+    {
+      accessToken: runtimeConfig.public.contentfulPublicAccessToken,
+    },
+    {
+      type: 'plain',
+      defaults: {
+        spaceId:  runtimeConfig.public.contentfulSpace,
+        environmentId: 'master',
+      },
+    }
+  );
+
+  return {
+    provide: {
+      contentfulManager: scopedPlainClient,
+    },
+  };
+});
