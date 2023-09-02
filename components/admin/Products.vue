587a31d57dc7f60744847ef62589aec89cd8571e
Abdou <2000abdallah2014@gmail.com>
2023-09-02 12:26:16 +0100

    added patch/delete product functionality

--- components/admin/Products.vue
+++ components/admin/Products.vue
@@ -14,7 +14,8 @@
                     <tbody>
                         <tr v-for="property in modalProperties">
                             <td>{{ property.name }}</td> <input type="text" placeholder="Type here"
-                                class="input w-full max-w-xs input-sm" v-model="selectedProduct[property.value]" />
+                                class="input w-full max-w-xs input-sm"
+                                v-model="selectedProduct.fields[property.value]['en-US']" />
                             <td></td>
                         </tr>
                     </tbody>
@@ -24,12 +25,12 @@
                     <button class="btn" @click="showModal = false">
                         close
                     </button>
-                    <button class="btn  " @click="handlePatch(currentUid, modalObject)">
+                    <button class="btn  " @click="handlePatch">
 
                         <span class="pl-2">apply changes</span>
                     </button>
 
-                    <button class="btn  btn-error" @click="handleDelete(currentUid)">
+                    <button class="btn  btn-error" @click="handleDelete">
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="M10 11V17M14 11V17M4 7H20M19 7L18.133 19.142C18.0971 19.6466 17.8713 20.1188 17.5011 20.4636C17.1309 20.8083 16.6439 21 16.138 21H7.862C7.35614 21 6.86907 20.8083 6.49889 20.4636C6.1287 20.1188 5.90292 19.6466 5.867 19.142L5 7H19ZM15 7V4C15 3.73478 14.8946 3.48043 14.7071 3.29289C14.5196 3.10536 14.2652 3 14 3H10C9.73478 3 9.48043 3.10536 9.29289 3.29289C9.10536 3.48043 9 3.73478 9 4V7H15Z"
@@ -60,14 +61,37 @@ const modalProperties = [
 
 
 //product editing
-const selectedId = ref('')
+//const selectedId = ref('')
 const selectedProduct = ref()
 
-const handleModal = (id) => {
-    selectedId.value = id;
-    const findProductById = productStore.products.find((product) => product.sys.id === id)
-    selectedProduct.value = Object.assign({}, findProductById.fields)
+const handleModal = async (id) => {
+    //selectedId.value = id;
+    // const findProductById = productStore.products.find((product) => product.sys.id === id)
+    selectedProduct.value = await $contentfulManager.entry.get({ entryId: id })
+    console.log(selectedProduct.value)
     showModal.value = true
+
+}
+
+const handlePatch = async () => {
+    try {
+        console.log(selectedProduct.value.fields.price);
+        const res = await $contentfulManager.entry.publish({
+            entryId: selectedProduct.value.sys.id
+        }, selectedProduct.value)
+        console.log(res)
+        useAlertsStore().success("Done!")
+    }
+    catch {
+        useAlertsStore().error("An error occurred, please contact teh dev team")
+    }
+}
+
+
+const handleDelete = () => {
+    $contentfulManager.entry.delete({
+        entryId: selectedProduct.value.sys.id
+    })
 }
 </script>
 <style >
