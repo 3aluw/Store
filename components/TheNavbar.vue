9faf985e6949e528483fdd7ecc7832bd2e6fbb74
Abdou <2000abdallah2014@gmail.com>
2025-06-15 10:21:50 +0100

    Add SVG icon for product representation; update logged-in user display logic in navbar

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -130,7 +130,7 @@ watch(locale, (newLocale) => {
       <!--UI for logged In Users-->
       <div v-else class="dropdown dropdown-end">
         <label tabindex="0" class="btn btn-sm ml-5">
-          <button>{{ loggedInUser.name ?? loggedInUser.email }}</button>
+          <button>{{ loggedInUser.name.length ? loggedInUser.name : loggedInUser.email }}</button>
         </label>
         <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
           <li>
